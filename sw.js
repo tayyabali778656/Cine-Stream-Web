48e69e00f2b71c31c983bdba01f1a6cd6868b2ef
tayyabali77865-tech <tayyabali77865@gmail.com>
2026-08-03 14:51:23 +0500

    chore: push user custom revisions for index.html and sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,6 @@
-try {
-  self.options = {
-      "domain": "5gvci.com",
-      "zoneId": 11462725
-  }
-  self.lary = ""
-  importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
-} catch (e) {
-  console.warn('Ad network service worker script import blocked:', e);
+self.options = {
+  "domain": "5gvci.com",
+  "zoneId": 11462725
 }
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
